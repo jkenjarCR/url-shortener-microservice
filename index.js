b4e404358680d583b748656d58acee3218e01e42
Jasmin Kenjar <jasminkenjar@2023-JasminKenjar.lan>
2024-11-27 17:10:06 -0600

    A

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const shortid = require("shortid");
 require("dotenv").config();
-let bodyParser = require("body-parser");
-const dns = require('dns');
-const shortid = require('shortid'); // For generating short unique IDs
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
 var url_data;
 
-// Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+// API Requests
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl", function (req, res) {
   try {
     var hostname = new URL(req.body.url).hostname;
-    dns.lookup(hostname, function(err) {
-      if(err) {
-        res.json({ error: 'invalid url' });
+    dns.lookup(hostname, function (err) {
+      if (err) {
+        res.json({ error: "invalid url" });
       } else {
         url_data = {
           original_url: req.body.url,
-          short_url: shortid.generate()
+          short_url: shortid.generate(),
         };
         res.json(url_data);
       }
     });
-  } catch(err) {
-    res.json({ error: 'invalid url' });
+  } catch (err) {
+    res.json({ error: "invalid url" });
   }
 });
 
-app.get('/api/shorturl/:short_url', function(req, res) {
-  if(req.params.short_url == url_data.short_url) {
+app.get("/api/shorturl/:short_url", function (req, res) {
+  if (req.params.short_url == url_data.short_url) {
     res.redirect(url_data.original_url);
   }
 });
 
-app.listen(port, function() {
+// Listen to Port
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
